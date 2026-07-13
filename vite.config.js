162fb1d44e1a21307cadc0cb491757d123fe002d
rankoverflow-io <rankoverflow.io@gmail.com>
2026-07-13 18:10:01 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         services: resolve(__dirname, 'services.html'),
+        aiServices: resolve(__dirname, 'ai-services.html'),
         about: resolve(__dirname, 'about.html'),
         contact: resolve(__dirname, 'contact.html'),
         score: resolve(__dirname, 'score.html'),
         casestudies: resolve(__dirname, 'casestudies.html'),
-        aiServices: resolve(__dirname, 'ai-services.html'),
         notFound: resolve(__dirname, '404.html')
       }
     }
